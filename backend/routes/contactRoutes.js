5536d1ef1cbd6766dbe4a885021322091c15dd70
TERENCE-SMITH <terence.smith@va.gov>
2025-04-10 15:09:25 -0400

    Updates

--- backend/routes/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitContact, getContacts } = require('../controllers/contactController');
-
-router.route('/').post(submitContact).get(getContacts);
-
-module.exports = router;
